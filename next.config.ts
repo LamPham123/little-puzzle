b7fb7935afbfac8729d2771dc0b7bbb4f2218f33
LamPham123 <LamPham705@gmail.com>
2024-11-12 14:42:34 -0800

    first try to put into rep

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    disableStaticImages: false,
+  },
 };
 
 export default nextConfig;
+
